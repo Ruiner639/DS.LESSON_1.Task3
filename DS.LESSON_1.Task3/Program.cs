e2ee7ba31447d3e04e086402211830df53f49d35
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2021-01-30 13:17:43 +0200

    Добавьте файлы проекта.

---
+++ DS.LESSON_1.Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DS.LESSON_1.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Display the pattern like pyramid using the alphabet.";
+            string[] list = str.Split(" ");
+            int a = list.Length - 1;
+            while (a != -1)
+            {
+                Console.WriteLine(list[a]);
+                a--;
+            }
+        }
+    }
+}
